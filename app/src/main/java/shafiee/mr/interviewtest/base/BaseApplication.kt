681ba3d4e16c0c93a345da1ec8e4d8e057528b0c
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2020-11-24 19:44:06 +0330

    add applicationInjector method to BaseApplication.kt & successfully built

--- app/src/main/java/shafiee/mr/interviewtest/base/BaseApplication.kt
+++ app/src/main/java/shafiee/mr/interviewtest/base/BaseApplication.kt
@@ -1,7 +1,14 @@
 package shafiee.mr.interviewtest.base
 
+import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import shafiee.mr.interviewtest.di.DaggerAppComponent
 
 abstract class BaseApplication : DaggerApplication() {
 
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder()
+            .application(this)
+            .build();
+    }
 }
