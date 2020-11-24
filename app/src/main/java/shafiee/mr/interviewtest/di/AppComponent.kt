05d42c817ab0e2d0b21bb4ec2fa6f257c9d3caf7
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2020-11-24 19:45:38 +0330

    create appModule with retrofit and okHttp providers

--- app/src/main/java/shafiee/mr/interviewtest/di/AppComponent.kt
+++ app/src/main/java/shafiee/mr/interviewtest/di/AppComponent.kt
@@ -12,6 +12,7 @@ import javax.inject.Singleton
 @Component(
         modules = [
             AndroidSupportInjectionModule::class,
+            AppModule::class,
             ActivityBuildersModule::class
         ]
 )
---
+++ app/src/main/java/shafiee/mr/interviewtest/di/AppModule.kt
@@ -0,0 +1,55 @@
+package shafiee.mr.interviewtest.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import shafiee.mr.interviewtest.Constants
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+object AppModule {
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): Interceptor {
+        return HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGSONInstance(): Gson {
+        return GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(client: OkHttpClient, gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(client)
+            .build();
+    }
+}
