c4beb2777b85a7318296328b85b7de12533c86da
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2020-11-25 00:49:25 +0330

    remove abstract

--- app/src/main/java/shafiee/mr/interviewtest/base/BaseApplication.kt
+++ app/src/main/java/shafiee/mr/interviewtest/base/BaseApplication.kt
@@ -4,7 +4,7 @@ import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import shafiee.mr.interviewtest.di.DaggerAppComponent
 
-abstract class BaseApplication : DaggerApplication() {
+class BaseApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder()
