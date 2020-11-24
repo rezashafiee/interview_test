9bba7e41954c6081ee68f2118f5a9cf725d33008
Shafie.MohamadReza <shafie.mohamadreza@okala.com>
2020-11-24 15:54:17 +0330

    Constants.kt added

---
+++ app/src/main/java/shafiee/mr/interviewtest/Constants.kt
@@ -0,0 +1,7 @@
+package shafiee.mr.interviewtest
+
+object Constants {
+    const val BASE_URL = "https://api.foursquare.com/v2/"
+    const val CLIENT_ID = "VYZQ00MFONFUHLLQ5L14VYJ2BHWC0EWOKAITRLERC5SA2RDG"
+    const val CLIENT_SECRET = "MW3S5X1CU05HDTUYG53WLENSTM0CF24BJ4KQMGGEDDAF2ROO"
+}
