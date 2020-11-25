90df13428f33a95aca43ec9bcca48aaac68fc990
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2020-11-25 20:19:52 +0330

    CommonExtensions is added

---
+++ app/src/main/java/shafiee/mr/interviewtest/utils/CommonExtensions.kt
@@ -0,0 +1,12 @@
+package shafiee.mr.interviewtest.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
