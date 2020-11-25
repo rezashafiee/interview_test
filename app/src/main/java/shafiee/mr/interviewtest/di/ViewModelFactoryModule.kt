21ee7cb3677294eb16860913ce161f843131693b
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2020-11-26 02:28:03 +0330

    change ViewModelProviderFactory and its package

--- app/src/main/java/shafiee/mr/interviewtest/di/ViewModelFactoryModule.kt
+++ app/src/main/java/shafiee/mr/interviewtest/di/ViewModelFactoryModule.kt
@@ -3,7 +3,7 @@ package shafiee.mr.interviewtest.di
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
-import shafiee.mr.interviewtest.viewmodels.ViewModelProviderFactory
+import shafiee.mr.interviewtest.viewmodel.ViewModelProviderFactory
 
 @Module
 abstract class ViewModelFactoryModule {
